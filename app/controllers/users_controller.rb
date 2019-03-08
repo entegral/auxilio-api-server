d56214276d7865ada6311bf35bdec9c32db2b255
entegral <e2robby@gmail.com>
2019-03-08 10:13:11 -0800

    add enhanced api functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,36 @@
 class UsersController < ApplicationController
 
   def index
-    @users = {"1": {
-      first_name: 'rob',
-      last_name: 'b',
-      email: 'email@gmail.com',
-      first_year_attended: 2013,
-      first_year_worked: 2013
-      }
-    }
+    @users = User.all
     json_response(@users)
   end
 
+    def show
+    @user = User.find(params[:uid])
+    json_response(@user)
+  end
+
+  def create
+    @user = User.create(user_params)
+    json_response(@user)
+  end
+
+  def update
+    @user = User.find(params[:uid])
+    @user.update(user_params)
+  end
+
+  def destroy
+    @user = User.find(params[:uid])
+    @user.destroy
+  end
+
   private
-  def json_response(object)
-    render json: object, status: :ok
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :uid, :first_year_enrolled, :first_year_worked)
   end
 end
