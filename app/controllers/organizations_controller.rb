16384204ff9846ea9632406d8110e2421f52e0d2
entegral <e2robby@gmail.com>
2019-03-11 15:15:00 -0700

    configure api endpoint to add new org for a user

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,54 @@
+class OrganizationsController < ApplicationController
+
+  def index
+    @user = User.find_by!(uid: api_params[:requester_uid])
+    if @user
+      @organizations = @user.organizations
+      json_response(@organizations)
+    else
+      render json: { message: 'Authentication failed'}
+    end
+  end
+
+  def show
+    if User.find_by!(uid: api_params[:requester_uid])
+      @organization = Organization.find_by!(uid: api_params[:uid])
+      json_response(@organization)
+    else
+      render json: { message: 'Authentication failed'}
+    end
+  end
+
+  def create
+    @user = User.find_by!(uid: api_params[:requester_uid])
+    if @user
+      org_params = {:uid=> api_params[:uid], :name=>api_params[:name]}
+      @user.organizations.create!(org_params)
+      json_response(@user.organizations, :created)
+    else
+      render json: { message: 'Authentication failed'}
+    end
+  end
+
+  def update
+    @organization = Organization.find_by!(uid: api_params[:uid])
+    if @organization.update!(api_params)
+      render status: 200, json: @user
+    end
+  end
+
+  def destroy
+    if User.find_by!(uid: api_params[:requester_uid])
+      @organization = Organization.find_by!(uid: api_params[:uid])
+      if @organization.destroy
+        render status: 200, json: { message: "User deleted sucessfully"}
+      end
+    else
+      render json: { message: 'Authentication failed'}
+    end
+  end
+
+  def api_params
+    params.permit(:requester_uid, :name, :uid)
+  end
+end
